4ea11f592ca132dfc03a70de2fe6fd82322ca5a6
Dustin Schau <dustinschau@gmail.com>
2017-12-17 20:34:44 -0600

    chore: fix eslint error

--- src/util/form.js
+++ src/util/form.js
@@ -65,10 +65,12 @@ export function sendEmail(url = FORM_URL) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(form)
-    }).then(
-      response =>
-        response.status === 302 ? null : Promise.reject('Failed form response')
-    ),
+    }).then(response => {
+      if (response.status === 302) {
+        return null;
+      }
+      return Promise.reject('Failed form submission');
+    }),
     new Promise((resolve, reject) => {
       setTimeout(() => reject('Service call failed'), 5000);
     })
